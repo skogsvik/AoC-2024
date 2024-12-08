58c4a62366d1343221341c9212cb4143ec75c588
Erik Silfverswärd <21970817+skogsvik@users.noreply.github.com>
2024-12-08 19:54:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Python solutions for [2024 Advent of Code](https://adventofcode.com/2024/)
---
+++ src/template.py
@@ -0,0 +1,11 @@
+TEST_1_INPUT = TEST_2_INPUT = """"""
+TEST_1_ANSWER = 0
+TEST_2_ANSWER = 0
+
+
+def part_1(data):
+    pass
+
+
+def part_2(data):
+    pass
